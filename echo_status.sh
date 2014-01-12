bd3b67d54537f2d58a644b069b9f956f64232d53
Georgi D. Sotirov <gdsotirov@dir.bg>
2014-01-12 09:24:14 +0000

    Add echo status in a separate file

---
+++ echo_status.sh
@@ -0,0 +1,16 @@
+echo_done() {
+  echo "${C_GREEN}Done${C_NORMAL}"
+}
+
+echo_fail() {
+  echo "${C_RED}Failure${C_NORMAL}"
+}
+
+echo_status() {
+  if [ $? != 0 ]; then
+    fail
+  else
+    ok
+  fi
+}
+
